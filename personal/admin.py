2781b22ae0c22329b7da265e76bb0ca2b88f24d7
MiguelWanderer <subterra.1999@gmail.com>
2026-09-25 01:27:36 -0300

    Crear modelos relacionados de personal

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Departamento, Empleado
+
+
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'descripcion')
+	search_fields = ('nombre', 'descripcion')
+	ordering = ('nombre',)
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'descripcion')
+	search_fields = ('nombre', 'descripcion')
+	ordering = ('nombre',)
+
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+	list_display = (
+		'rut',
+		'nombre',
+		'apellido',
+		'cargo',
+		'departamento',
+		'estado',
+	)
+	search_fields = ('rut', 'nombre', 'apellido', 'correo')
+	list_filter = ('cargo', 'departamento', 'estado')
+	ordering = ('apellido', 'nombre')
--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Departamento(models.Model):
+	nombre = models.CharField(max_length=100, unique=True)
+	descripcion = models.TextField(blank=True)
+
+	class Meta:
+		ordering = ['nombre']
+		verbose_name = 'departamento'
+		verbose_name_plural = 'departamentos'
+
+	def __str__(self):
+		return self.nombre
+
+
+class Cargo(models.Model):
+	nombre = models.CharField(max_length=100, unique=True)
+	descripcion = models.TextField(blank=True)
+
+	class Meta:
+		ordering = ['nombre']
+		verbose_name = 'cargo'
+		verbose_name_plural = 'cargos'
+
+	def __str__(self):
+		return self.nombre
+
+
+class Empleado(models.Model):
+	class Estado(models.TextChoices):
+		ACTIVO = 'ACTIVO', 'Activo'
+		INACTIVO = 'INACTIVO', 'Inactivo'
+
+	rut = models.CharField(max_length=12, unique=True)
+	nombre = models.CharField(max_length=100)
+	apellido = models.CharField(max_length=100)
+	correo = models.EmailField()
+	telefono = models.CharField(max_length=30)
+	fecha_ingreso = models.DateField()
+	cargo = models.ForeignKey(
+		Cargo,
+		on_delete=models.PROTECT,
+		related_name='empleados',
+	)
+	departamento = models.ForeignKey(
+		Departamento,
+		on_delete=models.PROTECT,
+		related_name='empleados',
+	)
+	estado = models.CharField(
+		max_length=8,
+		choices=Estado.choices,
+		default=Estado.ACTIVO,
+	)
+
+	class Meta:
+		ordering = ['apellido', 'nombre']
+		verbose_name = 'empleado'
+		verbose_name_plural = 'empleados'
+
+	def __str__(self):
+		return f'{self.nombre} {self.apellido}'
